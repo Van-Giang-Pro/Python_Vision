d55f72d4e30a53d357f22aafa5f3520847869ccb
Remote Repository <32243241+Van-Giang-Pro@users.noreply.github.com>
2026-02-06 17:07:53 -0500

    Add Basler camera trigger module implementation

---
+++ Trigger Basler Camera.py
@@ -0,0 +1,34 @@
+import cv2
+from pypylon import pylon
+
+defined_frame_width = 800
+try:
+    # Tạo một đối tượng camera để truy cập vào đôối tượng camera đầu tiên nhìn thấy
+    camera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+    print("Đã tìm thấy camera : ", camera.GetDeviceInfo().GetModelName())
+    camera.Open()
+    camera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly)
+    converter = pylon.ImageFormatConverter() # Tạo đối tượng chuyển đổi định dạng ảnh
+    converter.OutputPixelFormat = pylon.PixelType_BGR8packed # Chuyển đổi ảnh đầu ra thành BGR8 để cho opencv có thể xử lý
+    camera.LineSelector.Value = "Line1"
+    camera.LineMode.Value = "Input"
+    while camera.IsGrabbing():
+        grab_result = camera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+        input_status = camera.LineSelector.Value
+        if input_status:
+            print("Input Signal Detected")
+        else:
+            print("No Input Signal Detected")
+        if grab_result.GrabSucceeded() and input_status:
+            img_data = converter.Convert(grab_result).GetArray()
+            height, width, channels = img_data.shape
+            new_frame_width = defined_frame_width
+            new_frame_height = int(height * new_frame_width / width)
+            resized_image = cv2.resize(img_data, (new_frame_width, new_frame_height), interpolation=cv2.INTER_LINEAR)
+            cv2.imshow("Camera Viewer", img_data)
+except pylon.GenericException as e:
+    print("Đã xảy ra lỗi : ", e)
+except Exception as e:
+    print("Đã xảy ra lỗi không xác định : ", e)
+finally:
+    cv2.destroyAllWindows()
